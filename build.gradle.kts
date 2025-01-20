b2679dfb8cffa7e8d25c1f27da8c769f76ea064b
Timothy <timothyrwashburn@gmail.com>
2025-01-19 19:09:15 -0800

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    id("java")
+    id("com.github.johnrengelman.shadow") version "7.0.0"
+}
+
+group = "dev.kyriji"
+version = ""
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("net.dv8tion:JDA:5.2.2")
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
+
+tasks {
+    jar {
+        manifest {
+            attributes["Main-Class"] = "dev.kyriji.discord.TritonDiscord"
+        }
+    }
+
+    shadowJar {
+        archiveClassifier.set("")
+    }
+}
---
+++ src/main/java/dev/kyriji/discord/TritonDiscord.java
@@ -0,0 +1,4 @@
+package dev.kyriji.discord;
+
+public class TritonDiscord {
+}
